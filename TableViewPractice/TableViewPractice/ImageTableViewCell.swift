fa691c025c8bfba84d06738d5ea2a23a3d7aa40d
d2fault <d2fault@naver.com>
2020-06-21 13:09:06 +0900

    ✨ TableView 실습(Alamofire, SDWebImage 라이브러리 사용)

---
+++ TableViewPractice/TableViewPractice/ImageTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageTableViewCell.swift
+//  TableViewPractice
+//
+//  Created by Jiyoung Park on 2020/06/15.
+//  Copyright © 2020 d2fault. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+}
