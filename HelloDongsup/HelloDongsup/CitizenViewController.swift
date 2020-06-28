e80e6bad9aa0063fe1713c9d365c2313daae3e3d
d2fault <d2fault@naver.com>
2020-06-28 20:21:59 +0900

    ✨ Citizen 출력 기능 추가

--- HelloDongsup/HelloDongsup/CitizenViewController.swift
+++ HelloDongsup/HelloDongsup/CitizenViewController.swift
@@ -12,15 +12,24 @@ import Kingfisher
 
 class CitizenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return citizenModel?.data.citizens.count ?? 0;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CitizenTableViewCell", for: indexPath) as! CitizenTableViewCell
+        let citizens = citizenModel?.data.citizens
+        cell.citizenName.text = citizens?[indexPath.row].name
+        cell.citizenCatchphrase.text = citizens?[indexPath.row].catchphrase
+        
+        let imageUrl = citizens?[indexPath.row].imageUrl
+        let url = URL(string: imageUrl!)
+        cell.citizenImage.kf.setImage(with: url)
+        
         return cell
     }
     
     @IBOutlet weak var tableView: UITableView!
+    var citizenModel: CitizenModel?
     override func viewDidLoad() {
         super.viewDidLoad()
         callCitizenApi()
@@ -30,8 +39,9 @@ class CitizenViewController: UIViewController, UITableViewDelegate, UITableViewD
         let baseURL = "https://moti.company"
         AF.request(baseURL + "/api/v2/citizens").responseJSON { response in
             let decoder = JSONDecoder()
-            let result = try! decoder.decode(CitizenModel.self, from: response.data!)
-            print(result)
+            let citizenModel = try! decoder.decode(CitizenModel.self, from: response.data!)
+            self.citizenModel = citizenModel
+            self.tableView.reloadData()
         }
     }
 }
